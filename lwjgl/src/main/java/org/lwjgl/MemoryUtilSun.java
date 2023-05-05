9e7e014d006a5fda0f77f420beb3c91302460787
Cat Core <arthur.koalas@free.fr>
2023-05-06 00:07:37 +0200

    hm

--- lwjgl/src/main/java/org/lwjgl/MemoryUtilSun.java
+++ lwjgl/src/main/java/org/lwjgl/MemoryUtilSun.java
@@ -31,13 +31,6 @@
  */
 package org.lwjgl;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.nio.Buffer;
-
-import sun.misc.Unsafe;
-
 /**
  * MemoryUtil.Accessor implementations that depend on sun.misc.
  * We use reflection to grab these, so that we can compile on JDKs
@@ -48,57 +41,5 @@ import sun.misc.Unsafe;
 final class MemoryUtilSun {
 
 	private MemoryUtilSun() {
-	}
-
-	/** Implementation using sun.misc.Unsafe. */
-	private static class AccessorUnsafe implements MemoryUtil.Accessor {
-
-		private final Unsafe unsafe;
-		private final long   address;
-
-		AccessorUnsafe() {
-			try {
-				unsafe = getUnsafeInstance();
-				address = unsafe.objectFieldOffset(MemoryUtil.getAddressField());
-			} catch (Exception e) {
-				throw new UnsupportedOperationException(e);
-			}
-		}
-
-		public long getAddress(final Buffer buffer) {
-			return unsafe.getLong(buffer, address);
-		}
-
-		private static Unsafe getUnsafeInstance() {
-			final Field[] fields = Unsafe.class.getDeclaredFields();
-
-			/*
-			Different runtimes use different names for the Unsafe singleton,
-			so we cannot use .getDeclaredField and we scan instead. For example:
-
-			Oracle: theUnsafe
-			PERC : m_unsafe_instance
-			Android: THE_ONE
-			*/
-			for ( Field field : fields ) {
-				if ( !field.getType().equals(Unsafe.class) )
-					continue;
-
-				final int modifiers = field.getModifiers();
-				if ( !(Modifier.isStatic(modifiers) && Modifier.isFinal(modifiers)) )
-					continue;
-
-				field.setAccessible(true);
-				try {
-					return (Unsafe)field.get(null);
-				} catch (IllegalAccessException e) {
-					// ignore
-				}
-				break;
-			}
-
-			throw new UnsupportedOperationException();
-		}
-
 	}
 }
